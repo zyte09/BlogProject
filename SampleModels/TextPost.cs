eb3141a2d887a88cd426f2d3f25600b5c47052b6
Allen <143308484+zyte09@users.noreply.github.com>
2024-10-19 09:58:30 +0800

    Update TextPost.cs
    orig

--- SampleModels/TextPost.cs
+++ SampleModels/TextPost.cs
@@ -3,7 +3,7 @@ namespace BlogProject.SampleModels
     public class TextPost
     {
         public int TextPostID { get; set; }
-        public string Contents { get; set; }
+        public string Content { get; set; }
         public int PostID { get; set; }
         public int AuthorID { get; set; }
     }
